2c5e744c37909416ab22967effe1fc34b4d427ad
amr.mohammed <amr.mohamed@eg.aictec.com>
2023-09-14 03:11:35 +0200

    edit footer project name

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">@lang('Copyright') &copy; 2023 <a class="ml-25" href="https://www.kafroasters.net/" target="_blank">@lang("Kaf Ecommerce")</a><span class="d-none d-sm-inline-block">, @lang('All rights Reserved')</span></span></p>
+    <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">@lang('Copyright') &copy; 2023 <a class="ml-25" href="#">@lang("Invoices")</a><span class="d-none d-sm-inline-block">, @lang('All rights Reserved')</span></span></p>
 </footer>
 <button class="btn btn-primary btn-icon scroll-top" type="button" style="position: fixed;display: none"><i data-feather="arrow-up"></i></button>
 <!-- END: Footer-->
